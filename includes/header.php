d73c56ceb82503e4d2cb1f530a5f8e80e79b67e6
bernardo1574 <40328735+bernardo1574@users.noreply.github.com>
2020-06-15 09:01:04 -0300

    alteração do data table

--- includes/header.php
+++ includes/header.php
@@ -15,8 +15,11 @@
     <link href="lib/css/form-validation.css" rel="stylesheet">
     <link rel="stylesheet" href="lib/css/style.css">
     <link rel="stylesheet" href="lib/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="lib/DataTables/datatables.min.css"/>
-    <script type="text/javascript" src="lib/DataTables/datatables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css"/>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
 
 </head>
 
